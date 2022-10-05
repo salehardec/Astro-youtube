c19a26788f974221918165342931ed1512bc427b
Алексей <salehardec@gmail.com>
2022-10-05 22:32:06 +0500

    first commit

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,22 @@
+import {IProduct} from '../types'
+import {useState} from 'preact/hooks'
+
+interface ProductCardProps {
+	product: IProduct
+}
+
+export default function ProductCard({product}: ProductCardProps) {
+	const [details, setDetails] = useState(false)
+	const toggle = ()=> setDetails(prev=>!prev)
+	return <div className= "border rounded mb-2 p-3">
+		<h2 className="text-lg">{product.title}</h2>
+		<p className="font-bold">{product.price}</p>
+
+		<a href={`/product/${product.id}`}>Open product</a>
+		<br />
+
+		<button onClick={toggle} className="border py-2 px-4 bg-yellow-400 text-white">Toggle description</button>
+
+		{details &&<p>{product.description}</p>}
+	</div>
+}
