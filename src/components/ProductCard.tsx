f8519f8708ce9f532903dd352ae5c29787dc4586
salehardec <salehardec@gmail.com>
2022-11-09 10:59:01 +0500

    добавил chakru для запуска из под wsl

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,17 +5,17 @@ interface ProductCardProps {
 	product: IProduct
 }
 
-export default function ProductCard ({ product }: ProductCardProps) {
+export default function ProductCard({ product }: ProductCardProps) {
 	const [details, setDetails] = useState(false)
 	const toggle = () => setDetails(prev => !prev)
 	return <div className="border border-slate-400 rounded mb-2 p-3">
 		<h2 className="text-lg">{product.title}</h2>
 		<p className="font-bold">{product.price}</p>
 
-		<a href={`/product/${product.id}`}>Open product</a>
+		<a href={`/product/${product.id}`}>Открыть</a>
 		<br />
 
-		<button onClick={toggle} className="border py-2 px-4 bg-yellow-400 text-white">Toggle description</button>
+		<button onClick={toggle} className="border py-2 px-4 bg-yellow-400 text-white">Раскрыть</button>
 
 		{details && <p>{product.description}</p>}
 	</div>
